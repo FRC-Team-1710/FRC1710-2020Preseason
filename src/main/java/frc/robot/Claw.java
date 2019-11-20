4e3cdf7a0917ab974808fb72789354e8daf328cf
Charlie Bergdall <44418298+TheChar@users.noreply.github.com>
2019-11-19 18:34:07 -0600

    updated piston values and added final keyword to the scale variable

--- src/main/java/frc/robot/Claw.java
+++ src/main/java/frc/robot/Claw.java
@@ -14,44 +14,40 @@ import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
  * The code for the claw will be posted here for use on the new robot.
  * 
  * @author (Charlie Bergdall)
- * @version (0.0.1) Swenson's Claw uses Neos (Spark MAX), solenoids,
+ * @version (0.0.4) Swenson's Claw uses Neos (Spark MAX), solenoids,
  */
 public class Claw {
     //Global Variables
-    public static DoubleSolenoid LPiston, RPiston;
+    public static DoubleSolenoid hatchIntakePiston;
     public static CANSparkMax claw;
     public static TalonSRX clawIntakeMotor;
-    private static double scaler = 0.25;
+    private static final double SCALER = 0.25;
     
     //This method should be run first and will initialize all important variables to make the claw work
     public static void initClaw() {
-        LPiston = new DoubleSolenoid(0, 1); //Creates LPiston | PARAMETERS (forwardChannel, ReverseChannel)
-        RPiston = new DoubleSolenoid(2, 5); //Creates RPiston | same as above
+        hatchIntakePiston = new DoubleSolenoid(2, 5); //Creates hatch intake piston
         claw = new CANSparkMax(1, MotorType.kBrushless); //Creates claw | need to change the first int param to motor assignment ID
         clawIntakeMotor = new TalonSRX(2); //Creates clawIntakeMotor motor | need to change the int param to motor assingment ID
     }
 
     //Sets pistons to the intake mode
     public static void openPiston() {
-        LPiston.set(Value.kReverse);
-        RPiston.set(Value.kReverse);
+        hatchIntakePiston.set(Value.kReverse);
     }
 
     //Sets pistons to the outtake mode
     public static void closePiston() {
-        LPiston.set(Value.kForward);
-        RPiston.set(Value.kForward);
+        hatchIntakePiston.set(Value.kForward);
     }
 
     //Sets pistons to neutral mode | saw this in last years code so I wasn't sure if I should add it or not (ClawControl)
     public static void neutralPiston() {
-        LPiston.set(Value.kOff);
-        RPiston.set(Value.kOff);
+        hatchIntakePiston.set(Value.kOff);
     }
 
     //Controls Claw rotation
     public static void clawRotation(double mechLeftTrigDemand, double mechRightTrigDemand) {
-        double clawArmDemand = (mechLeftTrigDemand - mechRightTrigDemand) * scaler;
+        double clawArmDemand = (mechLeftTrigDemand - mechRightTrigDemand) * SCALER;
         claw.set(clawArmDemand);
     }
 
